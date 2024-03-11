6e0e3f1e285759691eee2af56fbfac4a382d9fde
Pekkmen <secretumnotitia@gmail.com>
2024-03-12 00:08:54 +0100

    Started working on the menu, WIP: If the user clicks on the last level before completing everything else, a label should appear for 2 seconds

--- main.c
+++ main.c
@@ -5,9 +5,10 @@
 //------------------------------------------------------------------------------------------
 // Types and Structures Definition
 //------------------------------------------------------------------------------------------
-typedef enum GameScreen {TITLE, GAMEPLAY, ENDING } GameScreen;
+typedef enum GameScreen {TITLE, LEVEL_1, LEVEL_2, LEVEL_3, LEVEL_4, LEVEL_5, ENDING } GameScreen;
 
-void draw_level_buttons(Rectangle *, float, float);
+void draw_level_buttons(GameScreen*, int);
+bool draw_exit_button(GameScreen *);
 
 //------------------------------------------------------------------------------------
 // Program main entry point
@@ -23,13 +24,13 @@ int main(void)
 
     GameScreen currentScreen = TITLE;
 
+    // Count how many levels have been completed
+    int level_completed = 0;
+
     // TODO: Initialize all required variables and load all required data here!
 
     SetTargetFPS(60);               // Set desired framerate (frames-per-second)
     //--------------------------------------------------------------------------------------
-
-    Rectangle level_buttons[5];
-    float level_buttons_width = 250.0f, level_buttons_height = 140.0f;
 
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
@@ -40,32 +41,31 @@ int main(void)
         {
             case TITLE:
             {
-                // TODO: Update TITLE screen variables here!
-                // // Press enter to change to GAMEPLAY screen
-                // if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
-                // {
-                //     currentScreen = GAMEPLAY;
-                // }
-            } break;
-            case GAMEPLAY:
-            {
-                // TODO: Update GAMEPLAY screen variables here!
 
-                // Press enter to change to ENDING screen
-                if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
-                {
-                    currentScreen = ENDING;
-                }
+            } break;
+            case LEVEL_1:
+            {
+
+            } break;
+            case LEVEL_2:
+            {
+
+            } break;
+            case LEVEL_3:
+            {
+
+            } break;
+            case LEVEL_4:
+            {
+
+            } break;
+            case LEVEL_5:
+            {
+
             } break;
             case ENDING:
             {
-                // TODO: Update ENDING screen variables here!
 
-                // Press enter to return to TITLE screen
-                if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
-                {
-                    currentScreen = TITLE;
-                }
             } break;
             default: break;
         }
@@ -82,17 +82,48 @@ int main(void)
                 case TITLE:
                 {
                     // TODO: Draw TITLE screen here!
-                    DrawRectangle(0, 0, screenWidth, screenHeight, GREEN);
                     DrawText("TITLE SCREEN", 20, 20, 40, DARKGREEN);
-                    draw_level_buttons(level_buttons, level_buttons_width, level_buttons_height);
+                    draw_level_buttons(&currentScreen, level_completed);
 
                 } break;
-                case GAMEPLAY:
+                case LEVEL_1:
                 {
                     // TODO: Draw GAMEPLAY screen here!
                     DrawRectangle(0, 0, screenWidth, screenHeight, PURPLE);
-                    DrawText("GAMEPLAY SCREEN", 20, 20, 40, MAROON);
-                    DrawText("PRESS ENTER or TAP to JUMP to ENDING SCREEN", 130, 220, 20, MAROON);
+                    DrawText("LEVEL_1", 130, 220, 20, MAROON);
+                    draw_exit_button(&currentScreen);
+
+                } break;
+                case LEVEL_2:
+                {
+                    // TODO: Draw GAMEPLAY screen here!
+                    DrawRectangle(0, 0, screenWidth, screenHeight, PURPLE);
+                    DrawText("LEVEL_2", 130, 220, 20, MAROON);
+                    draw_exit_button(&currentScreen);
+
+                } break;
+                case LEVEL_3:
+                {
+                    // TODO: Draw GAMEPLAY screen here!
+                    DrawRectangle(0, 0, screenWidth, screenHeight, PURPLE);
+                    DrawText("LEVEL_3", 130, 220, 20, MAROON);
+                    draw_exit_button(&currentScreen);
+
+                } break;
+                case LEVEL_4:
+                {
+                    // TODO: Draw GAMEPLAY screen here!
+                    DrawRectangle(0, 0, screenWidth, screenHeight, PURPLE);
+                    DrawText("LEVEL_4", 130, 220, 20, MAROON);
+                    draw_exit_button(&currentScreen);
+
+                } break;
+                case LEVEL_5:
+                {
+                    // TODO: Draw GAMEPLAY screen here!
+                    DrawRectangle(0, 0, screenWidth, screenHeight, PURPLE);
+                    DrawText("LEVEL_5", 130, 220, 20, MAROON);
+                    draw_exit_button(&currentScreen);
 
                 } break;
                 case ENDING:
@@ -101,6 +132,7 @@ int main(void)
                     DrawRectangle(0, 0, screenWidth, screenHeight, BLUE);
                     DrawText("ENDING SCREEN", 20, 20, 40, DARKBLUE);
                     DrawText("PRESS ENTER or TAP to RETURN to TITLE SCREEN", 120, 220, 20, DARKBLUE);
+                    draw_exit_button(&currentScreen);
 
                 } break;
                 default: break;
@@ -121,19 +153,89 @@ int main(void)
     return 0;
 }
 
-void draw_level_buttons(Rectangle *level_buttons, float level_buttons_width, float level_buttons_height){
-    // GuiButton((Rectangle){.x = GetScreenWidth()/2 - level_buttons_width/2,
-    //                       .y = GetScreenHeight()/2 - level_buttons_height/2,
-    //                       .width = level_buttons_width,
-    //                       .height = level_buttons_height
-    // }, "1. pálya");
+void draw_level_buttons(GameScreen *current_screen, int level_completed){
+    Rectangle level_buttons[5];
+    float level_buttons_width = 250.0f, level_buttons_height = 140.0f;
+    // Padding under and top of the button
+    float padding = 50.0f;
+
+    Font font = GuiGetFont();
+    font.baseSize = 2.0f;
+    GuiSetFont(font);
+
+    int choosen_level = 0;
+
+    int frames_counter = 120;
+
     for(int i = 0 ; i < 5; i++){
         level_buttons[i] = (Rectangle){
             .x = GetScreenWidth()/2 - level_buttons_width/2,
-            .y = 100.0f + i*level_buttons_height,
+            .y = 100.0f + i*(level_buttons_height + padding),
             .width = level_buttons_width,
             .height = level_buttons_height
         };
-        GuiButton(level_buttons[i], TextFormat("%d. pálya", i+1));
+        if(GuiButton(level_buttons[i], TextFormat("%d. pálya", i+1))){
+            // Save the index of the level button the user clicked on
+            choosen_level = i+1;
+            break;
+        }
     }
+    // Switch to the level corresponding to the button clicked by the user
+    switch (choosen_level)
+    {
+    case 1:
+        *current_screen = LEVEL_1;
+        break;
+
+    case 2:
+        *current_screen = LEVEL_2;
+        break;
+
+    case 3:
+        *current_screen = LEVEL_3;
+        break;
+
+    case 4:
+        *current_screen = LEVEL_4;
+        break;
+
+    case 5:
+        if(4 <= level_completed){
+            *current_screen = LEVEL_5;
+        } else {
+            TODO: FIX THIS
+            frames_counter = 0;
+        }
+        break; 
+    
+    default:
+        break;
+    }
+    while(frames_counter < 120){
+        DrawText("Teljesítened kell az első négy pályát mielőtt elkezdhetnéd az utolsót!", 0, 0, 30.0f, BLACK);
+        frames_counter++;
+    }
+}
+
+bool draw_exit_button(GameScreen *current_screen){
+    // I have no idea how to set the font size for only one instance, so I save the relevant font then reload it after I draw parts with the temporary font size
+    Font real_font = GuiGetFont();
+    Font temp_font = real_font;
+    temp_font.baseSize = 3.0f;
+    GuiSetFont(temp_font);
+
+    float exit_button_width = 100.0f, exit_button_height = 100.0f;
+
+    Rectangle exit_button = (Rectangle){
+        .x = GetScreenWidth() - exit_button_width,
+        .y = 0.0f,
+        .width = exit_button_width,
+        .height = exit_button_height
+    };
+    if(GuiButton(exit_button, "Menü")){
+        *current_screen = TITLE;
+    }
+    
+    // Load back the original font
+    GuiSetFont(real_font);
 }
