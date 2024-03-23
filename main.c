3c14a489b6a64d5ee5a5ff5f846884d503a67231
Pekkmen <secretumnotitia@gmail.com>
2024-03-23 23:39:42 +0100

    Commited war crimes against John Carmack

--- main.c
+++ main.c
@@ -3,14 +3,32 @@
 #include "src/raygui.h"
 #include <math.h>
 
+#define AND 1
+#define OR 2
+#define NAND 3
+#define NOR 4
+#define XOR 5
+#define XNOR 6
+
 //------------------------------------------------------------------------------------------
 // Types and Structures Definition
 //------------------------------------------------------------------------------------------
 typedef enum GameScreen {TITLE, LEVEL_1, LEVEL_2, LEVEL_3, LEVEL_4, LEVEL_5, ENDING } GameScreen;
+typedef struct LogicGates {
+    Rectangle rec;
+    int input1;
+    Vector2 input1_pos;
+    int input2;
+    Vector2 input2_pos;
+    int output;
+    Vector2 output_pos;
+} LogicGates;
 
 void draw_level_buttons(GameScreen*, int, bool *);
 bool draw_exit_button(GameScreen *);
 bool draw_level_1(GameScreen *);
+void change_logic_values(LogicGates *, int);
+void reset_logic_value(LogicGates  *);
 
 //------------------------------------------------------------------------------------
 // Program main entry point
@@ -108,7 +126,7 @@ int main(void)
                 {
                     // TODO: Draw GAMEPLAY screen here!
                     DrawRectangle(0, 0, screenWidth, screenHeight, PURPLE);
-                    DrawText("LEVEL 1", screenWidth/2, screenHeight/2, 60, MAROON);
+                    DrawText("1 PÁLYA", 1200, 10, 60, MAROON);
                     draw_level_1(&currentScreen);
 
                 } break;
@@ -283,27 +301,23 @@ bool draw_exit_button(GameScreen *current_screen){
 }
 
 bool draw_level_1(GameScreen *currentScreen) {
-    static bool textures_loaded = false;
+    static bool textures_loaded = false, initialized = false;
     static bool electricity = false, switched_on = false;
-    static Texture2D lever_lever, lever_bottom, display_off, display_on, gate_AND, gate_OR, gate_NAND, gate_NOR, gate_XOR, gate_XNOR;
+    static Texture2D lever_lever, lever_bottom, display_off, display_on, lg[6];
     // Loading textures
     if(!textures_loaded){
         lever_lever = LoadTexture("assets/level_1/lever_lever.png");
         lever_bottom = LoadTexture("assets/level_1/lever_bottom.png");
         display_off = LoadTexture("assets/level_1/on_off_display_OFF.png");
         display_on = LoadTexture("assets/level_1/on_off_display_ON.png");
-        gate_AND = LoadTexture("assets/level_1/AND.png");
-        gate_OR = LoadTexture("assets/level_1/OR.png");
-        gate_NAND = LoadTexture("assets/level_1/NAND.png");
-        gate_NOR = LoadTexture("assets/level_1/NOR.png");
-        gate_XOR = LoadTexture("assets/level_1/XOR.png");
-        gate_XNOR = LoadTexture("assets/level_1/XNOR.png");
+        for(int i = 0; i < 6; i++){
+            lg[i] = LoadTexture(TextFormat("assets/level_1/lg_%d.png", i));
+        }
 
         textures_loaded = true;
     }
     // DEBUG
-    if(IsKeyPressed(KEY_S) && !electricity) electricity = true;
-    else if(IsKeyPressed(KEY_S) && electricity) electricity = false; 
+    if(IsKeyPressed(KEY_S)) electricity = !electricity;
 
     Vector2 lever_lever_size = (Vector2){lever_lever.width, lever_lever.height};
     Vector2 lever_bottom_size = (Vector2){lever_bottom.width, lever_bottom.height};
@@ -333,7 +347,6 @@ bool draw_level_1(GameScreen *currentScreen) {
     if(electricity) DrawTexturePro(display_on, display_src, display_rec, (Vector2){.x=0, .y=0}, 0.0f, WHITE);
     else DrawTexturePro(display_off, display_src, display_rec, (Vector2){.x=0, .y=0}, 0.0f, WHITE);
 
-
     // I think changing the origin point shifts the entire rectangle away, so and unshifted version should be used for detecting clicks on the lever
     Rectangle unshifted_lever_rec = (Rectangle){
         .x = 100.0f,
@@ -342,11 +355,6 @@ bool draw_level_1(GameScreen *currentScreen) {
         .height = lever_lever_size.y
     };
 
-    // ORIGINAL PLAN: There should be a circle at the end of the lever (where the knob is on the lever), so a click on it could be detected and 'realistic' feel of pulling the lever can be achieved
-    // static Vector2 p = (Vector2){.x = 360, .y = 955};
-    // static Vector2 l = (Vector2){.x = 570, .y = 750};
-    // static Vector2 c = (Vector2){.x = 570, .y = 750};
-    // float distance = sqrt(pow(l.x - p.x, 2) + pow(l.y - p.y, 2));
     static float rotation = 45.0f, d_rotation = 0.0f;
     float mouse_pos_x_pressed;
     // Check fif the left mouse button was pressed inside the rectangle of the lever
@@ -362,8 +370,6 @@ bool draw_level_1(GameScreen *currentScreen) {
         } else if(rotation < -45.0f && 0 < d_rotation) rotation += d_rotation;
         else if(electricity && 45.0f < rotation && d_rotation < 0) rotation += d_rotation;
     }
-
-    // DrawCircle(c.x, c.y, 32.0f, BLACK);
 
     if(rotation <= -44.0f) switched_on = true;
     else switched_on = false;
@@ -400,65 +406,49 @@ bool draw_level_1(GameScreen *currentScreen) {
     e_sources[3].output = 1;
     e_sources[5].output = 1;
 
-    for(int i = 0; i < 6; i++){
-        if(e_sources[i].output == 0)
-            DrawRectanglePro(e_sources[i].rec, (Vector2){.x=0.0f, .y=0.0f}, 0.0f, DARKGREEN);
-        else if(e_sources[i].output == 1)
-            DrawRectanglePro(e_sources[i].rec, (Vector2){.x=0.0f, .y=0.0f}, 0.0f, GREEN);
-        else
-            DrawRectanglePro(e_sources[i].rec, (Vector2){.x=0.0f, .y=0.0f}, 0.0f, RED);
-    }
-
-    typedef struct LogicGates {
-        Rectangle rec;
-        int input1;
-        Vector2 input1_pos;
-        int input2;
-        Vector2 input2_pos;
-        int output;
-        Vector2 output_pos;
-    } LogicGates;
 
     static LogicGates logic_gates[6];
     // Initializing the logic gates placement rectangles
-    for(int i = 0; i < 6; i++){
-        logic_gates[i] = (LogicGates){
-            .input1 = -1,
-            .input2 = -1,
-            .output = -1,
-            .rec = (Rectangle){
-                .x = 800.0f,
-                .y = 350.0f,
-                .width = 100.0f,
-                .height = 100.0f
-            },
-            .input1_pos.x = logic_gates[i].rec.x + logic_gates[i].rec.width/3,
-            .input1_pos.y = logic_gates[i].rec.y,
-            .input2_pos.x = logic_gates[i].rec.x + 2*logic_gates[i].rec.width/3,
-            .input2_pos.y = logic_gates[i].rec.y,
-            .output_pos.x = logic_gates[i].rec.x + logic_gates[i].rec.width/2,
-            .output_pos.y = logic_gates[i].rec.y + logic_gates[i].rec.height
-        };
+    if(1){
+        for(int i = 0; i < 6; i++){
+            logic_gates[i] = (LogicGates){
+                .input1 = -1,
+                .input2 = -1,
+                .output = -1,
+                .rec = (Rectangle){
+                    .x = 800.0f,
+                    .y = 350.0f,
+                    .width = 100.0f,
+                    .height = 100.0f
+                },
+                .input1_pos.x = logic_gates[i].rec.x + logic_gates[i].rec.width/3,
+                .input1_pos.y = logic_gates[i].rec.y,
+                .input2_pos.x = logic_gates[i].rec.x + 2*logic_gates[i].rec.width/3,
+                .input2_pos.y = logic_gates[i].rec.y,
+                .output_pos.x = logic_gates[i].rec.x + logic_gates[i].rec.width/2,
+                .output_pos.y = logic_gates[i].rec.y + logic_gates[i].rec.height
+            };
+        }
+        logic_gates[0].rec.x = 900;
+        logic_gates[1].rec.x = 1300;
+        logic_gates[2].rec.x = 1700;
+        logic_gates[3].rec.x = 1100;
+        logic_gates[3].rec.y = 550;
+        logic_gates[4].rec.x = 1500;
+        logic_gates[4].rec.y = 550;
+        logic_gates[5].rec.x = 1300;
+        logic_gates[5].rec.y = 750;
     }
-    logic_gates[0].rec.x = 900;
-    logic_gates[1].rec.x = 1300;
-    logic_gates[2].rec.x = 1700;
-    logic_gates[3].rec.x = 1100;
-    logic_gates[3].rec.y = 550;
-    logic_gates[4].rec.x = 1500;
-    logic_gates[4].rec.y = 550;
-    logic_gates[5].rec.x = 1300;
-    logic_gates[5].rec.y = 750;
+    // // Setting up logic_gates in/outputs 
+    // for(int i = 0; i < 6; i++){
+    //     logic_gates[i].input1_pos.x = logic_gates[i].rec.x + logic_gates[i].rec.width/3;
+    //     logic_gates[i].input1_pos.y = logic_gates[i].rec.y;
+    //     logic_gates[i].input2_pos.x = logic_gates[i].rec.x + 2*logic_gates[i].rec.width/3;
+    //     logic_gates[i].input2_pos.y = logic_gates[i].rec.y;
+    //     logic_gates[i].output_pos.x = logic_gates[i].rec.x + logic_gates[i].rec.width/2;
+    //     logic_gates[i].output_pos.y = logic_gates[i].rec.y + logic_gates[i].rec.height;
+    // }
 
-    // Drawing the logic gates placement rectangles
-    for(int i = 0; i < 6; i++){
-        if(logic_gates[i].output == 0)
-            DrawRectanglePro(logic_gates[i].rec, (Vector2){.x=0.0f, .y=0.0f}, 0.0f, DARKGREEN);
-        else if(logic_gates[i].output == 1)
-            DrawRectanglePro(logic_gates[i].rec, (Vector2){.x=0.0f, .y=0.0f}, 0.0f, GREEN);
-        else 
-            DrawRectanglePro(logic_gates[i].rec, (Vector2){.x=0.0f, .y=0.0f}, 0.0f, RED);
-    }
 
     // The main output of the circuit
     typedef struct E_Output {
@@ -469,7 +459,7 @@ bool draw_level_1(GameScreen *currentScreen) {
         Vector2 output_pos;
     } E_Output;
 
-    E_Output e_output = {
+    static E_Output e_output = {
         .rec = (Rectangle){
             .x = 500.0f,
             .y = 1040.0f,
@@ -478,18 +468,12 @@ bool draw_level_1(GameScreen *currentScreen) {
         },
         .input = -1,
         .output = -1,
-        .input_pos.x = e_output.rec.x + e_output.rec.width/2,
-        .input_pos.y = e_output.rec.y,
-        .output_pos.x = e_output.rec.x + e_output.rec.width/2,
-        .output_pos.y = e_output.rec.y + e_output.rec.height
+        .input_pos.x = 550.0f,
+        .input_pos.y = 1040.0f,
+        .output_pos.x = 550.0f,
+        .output_pos.y = 1090.0f
     };
 
-    if(e_output.output == 0)
-        DrawRectanglePro(e_output.rec, (Vector2){.x=0.0f, .y=0.0f}, 0.0f, GRAY);
-    else if(e_output.output == 1) 
-        DrawRectanglePro(e_output.rec, (Vector2){.x=0.0f, .y=0.0f}, 0.0f, GREEN);
-    else 
-        DrawRectanglePro(e_output.rec, (Vector2){.x=0.0f, .y=0.0f}, 0.0f, RED);
 
     // Initializing the wires connecting the logic gates
     typedef struct Wire {
@@ -498,7 +482,7 @@ bool draw_level_1(GameScreen *currentScreen) {
         Vector2 output_pos;
     } Wire;
 
-    Wire wires[13];
+    static Wire wires[13];
     for(int i = 0; i < 13; i++){
         wires[i] = (Wire){
             .status = -1,
@@ -508,7 +492,7 @@ bool draw_level_1(GameScreen *currentScreen) {
     // Connecting and setting up the status of the wires connecting the first row of logic gates
     for(int i = 0; i < 6; i += 2) {
         // Determine the index of the logic gate
-        int gate_index = i / 2; 
+        int gate_index = i/2; 
 
         // First wire of the pair
         wires[i].input_pos.x = e_sources[i].output_pos.x;
@@ -558,16 +542,6 @@ bool draw_level_1(GameScreen *currentScreen) {
     wires[12].output_pos.x = e_output.input_pos.x;
     wires[12].output_pos.y = e_output.input_pos.y;
 
-    // Draw the wires
-    for(int i = 0; i < 13; i++){
-        if(wires[i].status == -1){
-            DrawLineBezier(wires[i].input_pos, wires[i].output_pos, 5, RED);
-        } else if(wires[i].status == 0){
-            DrawLineBezier(wires[i].input_pos, wires[i].output_pos, 5, DARKGREEN);
-        } else if(wires[i].status == 1){
-            DrawLineBezier(wires[i].input_pos, wires[i].output_pos, 5, GREEN);
-        }
-    }
 
     // Draw the lever parts
     DrawTexturePro(lever_lever,
@@ -583,31 +557,103 @@ bool draw_level_1(GameScreen *currentScreen) {
                     0.0f,
                     WHITE);
 
-    Rectangle logic_gates_src = (Rectangle){
+    static Rectangle logic_gates_src = (Rectangle){
         .x = 0.0f,
         .y = 0.0f,
-        .width = gate_XNOR.width,
-        .height = gate_XNOR.height
+        .width = 855.0f,
+        .height = 1295.0f
     };
-    Rectangle logic_gates_recs[6];
-    for(int i = 0; i < 6; i++){
-        logic_gates_recs[i] = (Rectangle){
-            .x = 150.0f * i,
-            .y = 200.0f,
-            .width = 80.0f,
-            .height = 100.0f
-        };
+    static Rectangle logic_gates_recs[6], logic_gates_recs_original[6];
+    if(!initialized){
+        for(int i = 0; i < 6; i++){
+            logic_gates_recs[i] = (Rectangle){
+                .x = 150.0f * i + 50,
+                .y = 10.0f,
+                .width = 80.0f,
+                .height = 100.0f
+            };
+            logic_gates_recs_original[i] = logic_gates_recs[i];
+        }
+        initialized = true;
     }
-    
-    DrawTexturePro(gate_AND, logic_gates_src, logic_gates_recs[0], (Vector2){.x=0,.y=0}, 0, WHITE);
-    DrawTexturePro(gate_OR, logic_gates_src, logic_gates_recs[1], (Vector2){.x=0,.y=0}, 0, WHITE);
-    DrawTexturePro(gate_NAND, logic_gates_src, logic_gates_recs[2], (Vector2){.x=0,.y=0}, 0, WHITE);
-    DrawTexturePro(gate_NOR, logic_gates_src, logic_gates_recs[3], (Vector2){.x=0,.y=0}, 0, WHITE);
-    DrawTexturePro(gate_XOR, logic_gates_src, logic_gates_recs[4], (Vector2){.x=0,.y=0}, 0, WHITE);
-    DrawTexturePro(gate_XNOR, logic_gates_src, logic_gates_recs[5], (Vector2){.x=0,.y=0}, 0, WHITE);
-
+    // DEBUG
+    int asd = 0;
+    int moved_gate_index = -1;
     Vector2 pos = GetMousePosition();
     if(IsMouseButtonDown(MOUSE_BUTTON_LEFT)) DrawText(TextFormat("%.0f, %.0f", pos.x, pos.y), 600, 600, 60, BLACK);
+    // Check for clicks on the logic gates
+    if(IsMouseButtonDown(MOUSE_BUTTON_LEFT)){
+        for(int i = 0; i < 6; i++){
+            if(CheckCollisionPointRec(pos, logic_gates_recs[i])){
+                logic_gates_recs[i].x = pos.x - 30;
+                logic_gates_recs[i].y = pos.y - 30;
+                break;
+            }
+        }
+    } else if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON)){
+        for (int i = 0; i < 6; i++) {
+        bool gate_moved = false;
+            for (int j = 0; j < 6; j++) {
+                moved_gate_index = j;
+                // Check if any logic gate collides with any logic gates placeholder
+                if (CheckCollisionRecs(logic_gates_recs[i], logic_gates[j].rec)) {
+                    logic_gates_recs[i] = logic_gates[j].rec;
+                    gate_moved = true;
+                    moved_gate_index = -1;
+                    change_logic_values(&logic_gates[j], i);
+                    asd = logic_gates[j].output;
+                    break;
+                }
+            }
+            // If not, place it back to its original position
+            if(!gate_moved && moved_gate_index != -1){
+                reset_logic_value(&logic_gates[moved_gate_index]);
+                logic_gates_recs[i] = logic_gates_recs_original[i];
+            } 
+        }
+    }
+    // DEBUG
+    DrawText(TextFormat("%d", asd), 0, 0, 50, BLACK);
+
+    // Draw the wires
+    for(int i = 0; i < 13; i++){
+        if(wires[i].status == -1){
+            DrawLineBezier(wires[i].input_pos, wires[i].output_pos, 5, RED);
+        } else if(wires[i].status == 0){
+            DrawLineBezier(wires[i].input_pos, wires[i].output_pos, 5, DARKGREEN);
+        } else if(wires[i].status == 1){
+            DrawLineBezier(wires[i].input_pos, wires[i].output_pos, 5, GREEN);
+        }
+    }
+    for(int i = 0; i < 6; i++){
+        if(e_sources[i].output == 0)
+            DrawRectanglePro(e_sources[i].rec, (Vector2){.x=0.0f, .y=0.0f}, 0.0f, DARKGREEN);
+        else if(e_sources[i].output == 1)
+            DrawRectanglePro(e_sources[i].rec, (Vector2){.x=0.0f, .y=0.0f}, 0.0f, GREEN);
+        else
+            DrawRectanglePro(e_sources[i].rec, (Vector2){.x=0.0f, .y=0.0f}, 0.0f, RED);
+    }
+    // Drawing the logic gates placement rectangles
+    for(int i = 0; i < 6; i++){
+        if(logic_gates[i].output == 0)
+            DrawRectanglePro(logic_gates[i].rec, (Vector2){.x=0.0f, .y=0.0f}, 0.0f, DARKGREEN);
+        else if(logic_gates[i].output == 1)
+            DrawRectanglePro(logic_gates[i].rec, (Vector2){.x=0.0f, .y=0.0f}, 0.0f, GREEN);
+        else 
+            DrawRectanglePro(logic_gates[i].rec, (Vector2){.x=0.0f, .y=0.0f}, 0.0f, RED);
+    }
+
+    if(e_output.output == 0)
+        DrawRectanglePro(e_output.rec, (Vector2){.x=0.0f, .y=0.0f}, 0.0f, GRAY);
+    else if(e_output.output == 1) 
+        DrawRectanglePro(e_output.rec, (Vector2){.x=0.0f, .y=0.0f}, 0.0f, GREEN);
+    else 
+        DrawRectanglePro(e_output.rec, (Vector2){.x=0.0f, .y=0.0f}, 0.0f, RED);
+
+    // Draw the logic gates 
+    for(int i = 0; i < 6; i++){
+        DrawTexturePro(lg[i], logic_gates_src, logic_gates_recs[i], (Vector2){.x=0,.y=0}, 0, WHITE);
+    }
 
     // If the user has clicked on the Menu button, unload the textures
     if(draw_exit_button(currentScreen)){
@@ -615,13 +661,28 @@ bool draw_level_1(GameScreen *currentScreen) {
         UnloadTexture(lever_bottom);
         UnloadTexture(display_off);
         UnloadTexture(display_on);
-        UnloadTexture(gate_AND);
-        UnloadTexture(gate_OR);
-        UnloadTexture(gate_NAND);
-        UnloadTexture(gate_NOR);
-        UnloadTexture(gate_XOR);
-        UnloadTexture(gate_XNOR);
+        for(int i = 0; i < 6; i++){
+            UnloadTexture(lg[i]);
+        }
 
         textures_loaded = false;
     }
+}
+
+void change_logic_values(LogicGates *logic_gate, int logic_value){
+    switch (logic_value)
+    {
+    case AND:
+        if(logic_gate->input1 == 1 && logic_gate->input2 == 1) logic_gate->output = 1;
+        else if(logic_gate->input1 == -1 || logic_gate->input2 == -1) logic_gate->output = -1;
+        else logic_gate->output = 0;
+        break;
+    
+    default:
+        break;
+    }
+}
+
+void reset_logic_value(LogicGates *logic_gate){
+    // logic_gate->output = -1;
 }
